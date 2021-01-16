16f1f5575050526fbbd3ed2796d0c30c1922ea43
Aarushi-rohit <74097519+Aarushi-rohit@users.noreply.github.com>
2021-01-16 21:05:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("details").style.color = "blue";
+
+// document.getElementByldC'details").style.color = "blue"
+//methodl // var details;
+// details = document.getElementByldC'details") ;
+
+//method2
+var details = document.getElementById("details")
+
+details.style.color = "blue";
